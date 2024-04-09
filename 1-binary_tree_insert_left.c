cbfa9044221a61eda6f0626f5513e1fd7d575382
Pakizamutallim <62bq5tn@code.edu.az>
2024-04-09 11:55:40 +0000

    created 1st task file

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - inserts a node
+ * @parent: pointer to the node to insert
+ * @value: the value to store in the new node
+ * Return: Always 0.
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t node;
+
+	if (!parent)
+		return (NULL);
+	node = malloc(sizeof(binary_tree_t));
+
+	if (!node)
+		return (NULL);
+
+	if (parent->left == NULL)
+	{
+		parent->left = node;
+	}
+	else
+	{
+		new->left = parent->left;
+		parent->left = node;
+	}
+	return (node);
+}
