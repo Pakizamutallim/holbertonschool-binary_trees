eb3b18b52cd4a3b7d529ed88c990b09a09c13e08
Pakizamutallim <62bq5tn@code.edu.az>
2024-04-09 12:12:22 +0000

    created 1st task file

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,7 +16,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (!parent)
 		return (NULL);
-	node = malloc(sizeof(binary_tree_t));
+	node = binary_tree_node(parent, value);
 
 	if (node == NULL)
 		return (NULL);
