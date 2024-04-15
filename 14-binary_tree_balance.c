51f1babf7db496ef31314d4a861dbedde4b16383
AsmarJabbarova <bd6p3ncs2@code.edu.az>
2024-04-15 11:32:37 +0000

    created 14th task

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,9 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree.
+ *
+ * @tree: Pointer to the root node of the tree.
+ *
+ * Return: Balance factor of tree, if tree is NULL, return 0.
+ */
