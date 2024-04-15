c172b7342cb9caf328568847a84bae1e62520d80
AsmarJabbarova <bd6p3ncs2@code.edu.az>
2024-04-15 11:50:12 +0000

    betty style eroror

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -26,7 +26,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        if (!tree)
+	if (!tree)
 		return (0);
 	if (binary_tree_height(tree->left) > binary_tree_height(tree->right))
 		return (1 + binary_tree_height(tree->left));
