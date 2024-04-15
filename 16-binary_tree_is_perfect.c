34efcbf3ff76587158932c0153cde0c429dcb775
AsmarJabbarova <bd6p3ncs2@code.edu.az>
2024-04-15 11:57:27 +0000

    creted binary_tree_is_perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,3 +8,17 @@
  * Return: If a binary tree perfect return 1, otherwise 0.
  * If tree is NULL, return 0.
  */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	size_t checker, height;
+
+	if (!tree)
+		return (0);
+	height = binary_tree_height(tree);
+	checker = 2 << (height - 1);
+	if (checker == binary_tree_leaves(tree) || height == 0)
+		return (1);
+	return (0);
+}
+
