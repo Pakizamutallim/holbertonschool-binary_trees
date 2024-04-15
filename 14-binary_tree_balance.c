07f671574eca7b199541d448d69d92b5019e5c27
AsmarJabbarova <bd6p3ncs2@code.edu.az>
2024-04-15 11:39:27 +0000

    creat binary_tree_balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,3 +7,10 @@
  *
  * Return: Balance factor of tree, if tree is NULL, return 0.
  */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+}
